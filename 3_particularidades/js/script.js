de52f6bfe43e5d462227aa084d01b865426b5164
Matheus Mendonça <matheussilvamendonca90@gmail.com>
2024-10-30 22:42:26 -0400

    particularidades do javascript

---
+++ 3_particularidades/js/script.js
@@ -0,0 +1,20 @@
+console.log("Primeiro");
+
+console.log("Último");
+
+var a = 1;
+
+a = 'teste'; 
+
+console.log(a);
+
+// Este é um comentário
+
+/* Este 
+    é 
+    um 
+    comentário 
+    de 
+    multiplas 
+    linhas
+*/
